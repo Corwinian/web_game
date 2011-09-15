145b893a676e6bcc438e319e2bcd8620fd02088b
Corwin <DartRavern@gmail.com>
2011-09-16 02:43:18 +1100

    create function "register user"

--- src/parser.py
+++ src/parser.py
@@ -7,7 +7,7 @@ Created on 11.09.2011
 
 import json
 import errors
-import db_commect
+import db_connect
 
 actions = dict(
                "register"= "register_user",
@@ -23,11 +23,13 @@ def parse_request(json):
         if not isinstance(json, dict):
             raise BadRequest("Json must be is object")
         try:
-            comand  = json["cmd"]
+            comand  = json.pop("cmd")
             try:
-                return actions[comand](json)
+                return actions[comand](**json)
             except (KeyError):
                 raise BadCommand("UnIndefined command")
+            except (TypeError):
+                raise BadCommand("bad command")
             except (RequestException) e:
                 raise e
         except (KeyError):
@@ -36,12 +38,16 @@ def parse_request(json):
     except (TypeError):
         raise BadRequest("Request is not json")
     
-def register_user(json):
+def register_user(user, password):
     try #gпотом могет из дб кидать
-        user = User(json)
-        
+        user = User(user, password)
+        userInDb = data_Base.query(User).filter_by(name = user.name).one()
+        if ()
+    except sqlalchemy.orm.exc.NoResultFound:
+        user = User(username, password)
+        dbi().add(user)
     except (KeyError):
-            raise BadCommand("badparams")   
+        raise BadCommand("bad params")   
     return
 
 def login_user(json):
